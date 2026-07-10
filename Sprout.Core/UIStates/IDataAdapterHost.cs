eb7910a07119e7d05b4e3d183c77f091b2dd1648
Boris <boris.borislavov@emerson.com>
2026-07-10 15:42:36 +0300

    Created IDataAdapterHost

---
+++ Sprout.Core/UIStates/IDataAdapterHost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IDataAdapter = Sprout.Core.Models.DataAdapters.IDataAdapter;
+
+namespace Sprout.Core.UIStates
+{
+    public interface IDataAdapterHost
+    {
+        IDataAdapter DataAdapter { get; set; }
+    }
+}
