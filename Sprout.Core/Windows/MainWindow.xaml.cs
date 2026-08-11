ee74b6a567203eb62ff94b67871d3708d9b349ce
Boris <boris.borislavov@emerson.com>
2026-08-11 16:00:23 +0300

    Set window title with file version

--- Sprout.Core/Windows/MainWindow.xaml.cs
+++ Sprout.Core/Windows/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using Sprout.Core.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Sprout.Core.Windows
 {
@@ -25,8 +15,10 @@ namespace Sprout.Core.Windows
             InitializeComponent();
 
             Content = mainView;
+            var fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly()!.Location).FileVersion;
+            Title = string.IsNullOrWhiteSpace(fileVersion) ? "Sprout" : $"Sprout - v{fileVersion}";
 
-            // Calculate 90% of the Primary Screen resolution
+            // Calculate 85% of the Primary Screen resolution
             this.Width = SystemParameters.PrimaryScreenWidth * 0.85;
             this.Height = SystemParameters.PrimaryScreenHeight * 0.85;
 
